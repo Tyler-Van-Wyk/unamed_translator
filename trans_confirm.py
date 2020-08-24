5aae523cc925524b47e1d9627798d54d541dc9e4
Tyler Van Wyk <63933153+Tyler-Van-Wyk@users.noreply.github.com>
2020-08-24 07:32:34 -0500

    Add files via upload

--- trans_confirm.py
+++ trans_confirm.py
@@ -20,10 +20,10 @@ for files in os.listdir(path):
     print('"' + files + '"\n' + ' will be translated to: \n' + '"' + trans_files + '"' + '\n proceed? (y/n)')
     while True:
         answer = input()
-        if answer == 'y':
+        if answer.lower() == 'y' or answer.lower() == 'yes':
             os.rename(files, trans_files)
             break
-        elif answer == 'n':
+        elif answer.lower() == 'n' or answer.lower() == 'no':
             break
         else:
             print('Invalid input. Please answer "y" or "n"')
